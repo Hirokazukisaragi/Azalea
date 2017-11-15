cd2ba7f8aa7d0b06ed45e0615440aaefe004ea00
Seno Hirokazu <hirokazukisaragi@gmail.com>
2017-11-15 13:48:28 +0900

    temp

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ var beam = {
   }
 }
 function init(){
-  alert(beam.getX());
+  var mycanvas = document.getElementById("mycanvas");
+  var myctx = mycanvas.getContext("2d");
+  for(var i = 0;i <= 20;i++){
+    for(var j = 0;j <= 20;j++){
+      myctx.drawImage(img, i*32, j*32);
+    }
+  }
 }
