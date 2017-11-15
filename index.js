a019f0447b26d4d9e78ba48611d4b016e86364ef
Seno Hirokazu <hirokazukisaragi@gmail.com>
2017-11-15 14:22:36 +0900

    temp

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ function init(){
   var myctx = mycanvas.getContext("2d");
   for(var i = 0;i <= 20;i++){
     for(var j = 0;j <= 20;j++){
-      myctx.drawImage(img, i*32, j*32);
+      if(i == 0 && j == 0){
+        myctx.drawImage(upimg,0,0);
+      }
+      else if(((i) % 2) == 0){
+        myctx.drawImage(bgimg, i*32, j*32);
+      }else{
+        myctx.drawImage(mtimg,i*32,j*32);
+      }
     }
   }
 }
